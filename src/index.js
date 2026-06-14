75b2e0402435c1cf0a562c375278083b8edf4b06
TusharSharma <rakeshsharmadj1008@gmail.com>
2026-06-14 12:49:18 +0530

    z

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 
 console.log("yoyo");
-const express = require("express");
+const express = require("express");g
 console.log("Hello")
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
