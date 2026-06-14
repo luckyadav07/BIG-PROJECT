4ed0f6f310576f705a22ebd7fb039c09d010b0ae
Lucky Yadav <luckyyadav61715@gmail.com>
2026-06-14 22:00:04 +0530

    SERVER RUNNING file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import connectdb from "./config/db.js";
 
-const express = require("express");
-console.log("Hello")
-console.log("Hello lucky")
-console.log("Hello lucky")
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.use(helmet());                              // hides server info from hackers
+app.use(cors());                                // allows frontend to talk to backend
+app.use(morgan("dev"));                         // logs every request in terminal
+app.use(express.json());                        // reads JSON data from requests
+app.use(express.urlencoded({ extended: true })); // reads form data from requests
+
+app.get("/", (req, res) => {
+    res.json({ message: "YEAH BABY SERVER IS RUNNING.........." })
+})
+
+connectdb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`SERVER RUNNING ka LIYE YHA pa jayye ${PORT}`)
+    })
+})
