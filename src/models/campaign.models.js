7b4902465232eba21a4ce4421c5125e2411535c5
Kunal Pal <your-email@example.com>
2026-06-16 20:06:21 +0530

    Refactor campaign model schema for improved structure and consistency

--- src/models/campaign.models.js
+++ src/models/campaign.models.js
@@ -1,27 +1,31 @@
 import mongoose,{Schema} from "mongoose";
 
-const campaignSchema=new Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+const campaignSchema=new Schema(
+    {
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        template:{
+            type:String,
+            required:true
+        },
+        recruiterEmails:{
+            type:[String],
+            required:true
+        },
+        schedule:{
+            type:Date
+        },
+        status:{
+            type:String,
+            enum:["pending","scheduled","sent","failed"],
+            default:"pending"
+        }
     },
-    template:{
-        type:String,
-        required:true
-    },
-    recruiterEmails:{
-        type:[String],
-        required:true
-    },
-    schedule:{
-        type:String
-    },
-    status:{
-        type:String,
-        default:"pending"
-
-    },
-
-})
+    {
+        timestamps: true
+    }
+)
 
 export default mongoose.model("Campaign",campaignSchema)
