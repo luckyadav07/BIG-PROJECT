9603aef010d9dfa25385b103ebb70d255ae5142e
Kunal Pal <your-email@example.com>
2026-06-26 16:03:39 +0530

    feat: add getCurrentUser endpoint and update auth middleware for user retrieval

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -78,5 +78,20 @@ const login = asyncHandler(async (req, res) => {
     )
 
 })
+const getCurrentUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
 
-export { register, login }
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    res.status(200).json(
+        new ApiResponse(
+            200,
+            { user },
+            "Current user fetched successfully"
+        )
+    );
+});
+
+export { register, login, getCurrentUser }
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,41 +1,28 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.models.js";
-import  asyncHandler  from "../utils/asyncHandler.js";
-import  ApiError  from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
 
 const authMiddle = asyncHandler(async (req, res, next) => {
-    // 1. Token nikalna (Bearer <token>)
     const token = req.headers.authorization?.split(" ")[1];
 
-    console.log("TOKEN:", token);
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    console.log("DECODED:", decoded);
-
     if (!token) {
-        throw new ApiError(401, "No token, access nhi milegaa laadle");
+        throw new ApiError(401, "No token provided");
     }
 
     try {
-        // 2. Token verify karna
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // 3. User find karna (password hata ke)
         const user = await User.findById(decoded.id).select("-password");
 
         if (!user) {
             throw new ApiError(401, "User not found");
         }
 
-        // 4. Request me user daalna taaki aage ke controllers isko use kar sake
         req.user = user;
-        
-        // 5. Agle function (controller) ko call karna
-        next();
 
+        next();
     } catch (error) {
-        // Agar token expire ho gaya ya galat hai
         throw new ApiError(401, "Invalid or expired token");
     }
 });
